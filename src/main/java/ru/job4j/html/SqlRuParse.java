2966ea9b34a26096dfd7cb368df2b1b6c3354e84
= <=afoni41985@yandex.ru>
2021-11-09 23:21:08 +0300

    2.3. Загрузка деталей поста. [#285212 #226588]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -9,7 +9,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public class SqlRuParse {
-    public static List<Post> detailHtml(String url) throws IOException {
+    public static Post detailHtml(String url) throws IOException {
         SqlRuDateTimeParser timeParser = new SqlRuDateTimeParser();
         Document doc = Jsoup.connect(url).get();
         String link = "https://www.sql.ru/forum/1325330/lidy-be-fe-senior-cistemnye-analitiki"
@@ -18,7 +18,7 @@ public class SqlRuParse {
         String description = doc.select(".msgBody").get(1).text();
         String time = doc.selectFirst(".msgFooter").text();
         LocalDateTime created = timeParser.parse(time.substring(0, time.indexOf("[")));
-        return List.of(new Post(title, link, description, created));
+        return new Post(title, link, description, created);
     }
 
     public static void main(String[] args) throws Exception {
