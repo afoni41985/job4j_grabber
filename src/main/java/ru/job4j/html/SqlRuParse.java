1ac5d3b2356d3832ac8f63ef1e061220dec49590
= <=afoni41985@yandex.ru>
2021-11-09 16:06:28 +0300

    2.3. Загрузка деталей поста. [#285212]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -2,20 +2,28 @@ package ru.job4j.html;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
+import ru.job4j.grabber.utils.SqlRuDateTimeParser;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.List;
 
 public class SqlRuParse {
+    public static List<Post> detailHtml(String url) throws IOException {
+        SqlRuDateTimeParser timeParser = new SqlRuDateTimeParser();
+        Document doc = Jsoup.connect(url).get();
+        String link = "https://www.sql.ru/forum/1325330/lidy-be-fe-senior-cistemnye-analitiki"
+                + "-qa-i-devops-moskva-do-200t";
+        String title = doc.selectFirst(".messageHeader").text();
+        String description = doc.select(".msgBody").get(1).text();
+        String time = doc.selectFirst(".msgFooter").text();
+        LocalDateTime created = timeParser.parse(time.substring(0, time.indexOf("[")));
+        return List.of(new Post(title, link, description, created));
+    }
+
     public static void main(String[] args) throws Exception {
-        for (int i = 0; i <= 5; i++) {
-            Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
-            Elements row = doc.select(".postslisttopic");
-            for (Element td : row) {
-                Element href = td.child(0);
-                System.out.println(href.attr("href"));
-                System.out.println(href.text());
-                System.out.println(td.parent().child(5).text());
-            }
-        }
+        System.out.println(detailHtml("https://www.sql.ru/forum/1325330/lidy-be-fe-"
+                + "senior-cistemnye-analitiki-qa-i-devops-moskva-do-200t"));
     }
 }
+
